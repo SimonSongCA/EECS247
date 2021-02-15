4ce2cffa2880b4d6f70c495c98350c7ddaa370e4
SimonSongCA <SimonSongCA@gmail.com>
2021-02-14 21:55:27 -0800

    MapReduce on a sample Covid dataset.

--- Project1/countMapper.m
+++
@@ -1,7 +0,0 @@
-function countMapper(data, info, intermKV)
-% Counts unique airline carrier names in each block.
-a = data.UniqueCarrier;
-c = num2cell(countcats(a));
-keys = categories(a);
-addmulti(intermKV, keys, c)
-end
--- Project1/countReducer.m
+++
@@ -1,9 +0,0 @@
-function countReducer(key, intermValIter, outKV)
-% Combines counts from all blocks to produce final counts.
-count = 0;
-while hasnext(intermValIter)
-    data = getnext(intermValIter);
-    count = count + data;
-end
-add(outKV, key, count)
-end
